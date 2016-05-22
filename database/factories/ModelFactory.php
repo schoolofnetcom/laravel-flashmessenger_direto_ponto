ef248ec923e87df4b90ca7302e552f5ca6d2d5ca
Luiz <argentinaluiz@gmail.com>
2016-05-22 10:47:20 -0300

    - Add to courses:    - migration    - factory    - call in DatabaseSeeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,48 @@
 |
 */
 
+$courses = [
+    'Iniciando com Angular 2',
+    'Angular 2 Intermediário',
+    'Angular 2 Avançado',
+    'ZF2 Essentials',
+    'Vue.js',
+    'Ember.js',
+    'Backbone.js',
+    'React.js',
+    'JQuery',
+    'JQuery-UI',
+    'PHP7',
+    'Python',
+    'Ruby',
+    'Ruby on Rails',
+    'C#',
+    'PHPStorm',
+    'Netbeans',
+    'HTML5',
+    'CSS3',
+    'Laravel 5',
+    'CakePHP',
+    'Silex',
+    'Yii2',
+    'FuelPHP',
+    'CodeIgniter',
+    'Symfony',
+    'Phalcon',
+    'Doctrine',
+    'Propel',
+    'Twig',
+    'Blade',
+    'Java',
+    'JPA',
+    'Hibernate',
+    'Scrum',
+    'TDD',
+    'PHPUnit',
+    'XP',
+    'DDD'
+];
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
@@ -19,3 +61,12 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Course::class, function (Faker\Generator $faker) use ($courses) {
+    return [
+        'title' => $courses[array_rand($courses, 1)],
+        'description' => $faker->paragraph(3),
+    ];
+});
+
+
