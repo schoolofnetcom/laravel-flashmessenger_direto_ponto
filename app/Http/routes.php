7c6b0f1c856fd379df5474d2ce0a7f20fba7fbbc
Luiz <argentinaluiz@gmail.com>
2016-05-22 12:28:32 -0300

    Remove middleware web from routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['middleware' => 'web'],function(){
-    Route::resource('courses','CoursesController');
-});
+
+Route::resource('courses', 'CoursesController');
 
