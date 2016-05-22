a46507fdf8c5d107317961353dc4ee1592b5e551
Luiz <argentinaluiz@gmail.com>
2016-05-22 12:11:07 -0300

    Add div.container-fluid to app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -56,8 +56,9 @@
 			</div>
 		</div>
 	</nav>
-
-	@yield('content')
+	<div class="container-fluid">
+		@yield('content')
+	</div>
 
 	<!-- Scripts -->
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
--- resources/views/courses/edit.blade.php
+++ resources/views/courses/edit.blade.php
@@ -1,45 +1,43 @@
 @extends('app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Update a Course</div>
-                    <div class="panel-body">
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Update a Course</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
-                        {{ Form::model($course, ['route' => ['courses.update', $course->id], 'method' => 'put', 'class' => 'form-horizontal']) }}
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label">Title</label>
-                                    <div class="col-md-6">
-                                        {{ Form::text('title', null, ['class' => 'form-control']) }}
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <label class="col-md-4 control-label">Description</label>
-                                    <div class="col-md-6">
-                                        {{ Form::textarea('description', null, ['class' => 'form-control']) }}
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <button type="submit" class="btn btn-primary btn-block">Submit</button>
-                                    </div>
-                                </div>
-                        {{ Form::close() }}
+                    {{ Form::model($course, ['route' => ['courses.update', $course->id], 'method' => 'put', 'class' => 'form-horizontal']) }}
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Title</label>
+                        <div class="col-md-6">
+                            {{ Form::text('title', null, ['class' => 'form-control']) }}
+                        </div>
                     </div>
+
+                    <div class="form-group">
+                        <label class="col-md-4 control-label">Description</label>
+                        <div class="col-md-6">
+                            {{ Form::textarea('description', null, ['class' => 'form-control']) }}
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-md-6 col-md-offset-4">
+                            <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                        </div>
+                    </div>
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
